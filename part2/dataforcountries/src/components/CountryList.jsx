4ee67ee33caa4e3ad816a54c0edbbb438ab48f18
kat-ki <glraskat@gmail.com>
2024-01-31 22:15:52 +0100

    feat: 2.18 done

--- part2/dataforcountries/src/components/CountryList.jsx
+++ part2/dataforcountries/src/components/CountryList.jsx
@@ -1,10 +1,6 @@
 export const CountryList = ({filteredCountries}) => {
     console.log(filteredCountries)
 
-    /*  const languages = filteredCountries.languages.map((lang) => {
-          return <li key={lang.iso639_1}>{lang.name}</li>
-      })*/
-
     return (
         <div>
             {
@@ -12,14 +8,17 @@ export const CountryList = ({filteredCountries}) => {
                     ? 'Too many matches. Please specify another filter'
                     : filteredCountries.length === 1
                         ? filteredCountries.map((c) => (
-                            <div key={c.name.common}>
+                            <div key={c.flag}>
                                 <h3>{c.name.common}</h3>
                                 <p>Capital: {c.capital}</p>
                                 <p>Area: {c.area}</p>
                                 <div>
-                                    {/*<ul>
-                                    {c.map((lang) => <li>{lang.name}</li>}
-                                </ul>*/}
+                                    <ul>
+                                        {Object.entries(c.languages).map(([key, value]) => (
+                                            <li key={key}>{value}</li>
+                                        ))}
+                                    </ul>
+
                                 </div>
                                 <p>Languages:</p>
 
@@ -27,7 +26,7 @@ export const CountryList = ({filteredCountries}) => {
                             </div>
                         ))
                         : filteredCountries.map((c) => (
-                            <div key={c.name.common}>
+                            <div key={c.flag}>
                                 <h3>{c.name.common}</h3>
                             </div>
                         ))
