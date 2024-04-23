ae1977a8a3b697d6d130da68fa800943a31ce0a7
kat-ki <glraskat@gmail.com>
2024-04-23 15:36:00 +0200

    5.4: added notifications for successful/unsuccessful operations

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -2,16 +2,20 @@ import {useState, useEffect} from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login.js'
+import Notification from "./components/Notification.jsx";
 
 const App = () => {
-    const [blogs, setBlogs] = useState([])
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [user, setUser] = useState(null)
+    const [blogs, setBlogs] = useState([]);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [user, setUser] = useState(null);
 
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
     const [url, setUrl] = useState('');
+
+    const [message, setMessage] = useState(null);
+    const [status, setStatus] = useState('')
 
     useEffect(() => {
         blogService.getAll().then(blogs =>
@@ -36,10 +40,12 @@ const App = () => {
             setUsername('')
             setPassword('')
         } catch (error) {
-            //error info with setTimeout
-            console.log(error)
+            setMessage('Wrong username or password')
+            setStatus('error')
+            setTimeout(() => {
+                setMessage(null)
+            }, 2000)
         }
-
     }
     const loginForm = () => (
         <form onSubmit={handleLogin}>
@@ -69,18 +75,62 @@ const App = () => {
         setUser(null)
     }
     const addBlog = async (event) => {
-        event.preventDefault()
+        event.preventDefault();
+        if (!title || !author || !url) {
+            setMessage('Title, author, and url must not be empty')
+            setStatus('error')
+            setTimeout(() => {
+                setMessage(null)
+            }, 2000)
+            return
+        }
+
         const newBlog = {title, author, url};
-        const response = await blogService.create(newBlog)
-        setBlogs([response, ...blogs])
-        setTitle('')
-        setAuthor('')
-        setUrl('')
+        try {
+            const response = await blogService.create(newBlog)
+            setMessage(`Added ${response.title}`)
+            setStatus('success')
+            setTimeout(() => {
+                setMessage(null)
+            }, 2000)
+            setBlogs([...blogs, response])
+            setTitle('')
+            setAuthor('')
+            setUrl('')
+        } catch (error) {
+            setMessage(error.message)
+            setStatus('error')
+            setTimeout(() => {
+                setMessage(null)
+            }, 2000)
+        }
     }
+    const createBlogForm = () => (
+        <form onSubmit={addBlog} style={{
+            marginBottom: '10px',
+            display: 'flex',
+            flexDirection: 'column',
+            width: '30%',
+            paddingBottom: '10px'
+        }}>
+            <label htmlFor="title">Title: </label>
+            <input type="text" name="title" value={title}
+                   onChange={({target}) => setTitle(target.value)}/>
+            <label htmlFor="author">Author: </label>
+            <input type="text" name="author" value={author}
+                   onChange={({target}) => setAuthor(target.value)}/>
+            <label htmlFor="url">URL: </label>
+            <input type="text" name="url" value={url} onChange={({target}) => setUrl(target.value)}/>
+
+            <button type="submit" style={{width: '50px', marginTop: '10px', padding: '5px'}}>Create
+            </button>
+        </form>
+    )
 
     return (
         <div>
             <h2 style={{margin: '20px'}}>Blogs</h2>
+            <Notification message={message} status={status}/>
             {user === null
                 ? loginForm()
                 : <div>
@@ -88,25 +138,7 @@ const App = () => {
                     <button onClick={handleLogout}>log out</button>
                     <div style={{margin: '10px'}}>
                         <h3>Add new blog</h3>
-                        <form onSubmit={addBlog} style={{
-                            marginBottom: '10px',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            width: '30%',
-                            paddingBottom: '10px'
-                        }}>
-                            <label htmlFor="title">Title: </label>
-                            <input type="text" name="title" value={title}
-                                   onChange={({target}) => setTitle(target.value)}/>
-                            <label htmlFor="author">Author: </label>
-                            <input type="text" name="author" value={author}
-                                   onChange={({target}) => setAuthor(target.value)}/>
-                            <label htmlFor="url">URL: </label>
-                            <input type="text" name="url" value={url} onChange={({target}) => setUrl(target.value)}/>
-
-                            <button type="submit" style={{width: '50px', marginTop: '10px', padding: '5px'}}>Create
-                            </button>
-                        </form>
+                        {createBlogForm()}
                     </div>
                     <div style={{margin: '10px'}}>
                         {blogs.map(blog =>
