27c331182fdcc09e49b5ff034ffb9c4c79af7968
kat-ki <glraskat@gmail.com>
2024-05-07 10:21:00 +0200

    5.16: added tests for BlogForm

--- part5/bloglist-frontend/test/BlogForm.test.jsx
+++ part5/bloglist-frontend/test/BlogForm.test.jsx
@@ -3,13 +3,6 @@ import {describe, expect, test} from 'vitest';
 import userEvent from '@testing-library/user-event'
 import BlogForm from "../src/components/BlogForm.jsx";
 
-const blog = {
-    id: '33',
-    title: 'testing title',
-    author: 'super author',
-    url: 'httpTest',
-    likes: 12,
-}
 describe('<BlogForm />', () => {
     test('form calls event handler when new blog is created', async () => {
         const createBlog = vi.fn();
