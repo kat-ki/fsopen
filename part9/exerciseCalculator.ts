d486fbd06c7630c6dd58bea5a660c7d3d758cd54
kat-ki <glraskat@gmail.com>
2024-09-30 15:08:34 +0200

    9.2: exercise calculator

---
+++ part9/exerciseCalculator.ts
@@ -0,0 +1,43 @@
+interface Result {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+}
+
+const calculateExercises = (hoursPerDay: number[], target: number): Result => {
+    const periodLength = hoursPerDay.length;
+    const trainingDays = hoursPerDay.filter(day => day > 0).length;
+    const totalHours = hoursPerDay.reduce((acc, curr) => acc + curr, 0);
+    const average = totalHours / periodLength;
+    const success = average >= target;
+
+    let rating;
+    let ratingDescription;
+
+    if (average >= target) {
+        rating = 3;
+        ratingDescription = 'Good job! You met your target.';
+    } else if (average >= target * 0.75) {
+        rating = 2;
+        ratingDescription = 'Not bad, but you can do better.';
+    } else {
+        rating = 1;
+        ratingDescription = 'You need to put in more effort.';
+    }
+
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average
+    };
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
