d11f0b0e66da746d7fd7cb91f30a71ab61a39b09
kat-ki <glraskat@gmail.com>
2024-10-10 17:18:10 +0200

    remove part7

--- part7-1/countries/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AppCountryHook from './AppCountryHook.jsx'
-import AppNotesUltimateHooks from "./AppNotes-UltimateHooks.jsx";
-
-//Note: render AppCountryHook for task 7.7: Country hook or render AppNotesUltimateHooks for task 7.8: Ultimate Hooks
-
-ReactDOM.createRoot(document.getElementById('root')).render(<AppNotesUltimateHooks/>)
