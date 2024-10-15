61258d7fb66e1e4c0967285c04085c771e72ade6
kat-ki <glraskat@gmail.com>
2024-10-15 12:24:42 +0200

    9.10: add get /api/diagnoses endpoint, fetched data

---
+++ part9/ilaris-flight-diaries/src/data/entries.ts
@@ -0,0 +1,16 @@
+import { DiaryEntry } from "../types";
+
+
+const diaryEntries: DiaryEntry[] = [
+    {
+        "id": 1,
+        "date": "2017-01-01",
+        "weather": "rainy",
+        "visibility": "poor",
+        "comment": "Pretty scary flight, I'm glad I'm alive"
+    },
+    // ...
+];
+
+
+export default diaryEntries;
